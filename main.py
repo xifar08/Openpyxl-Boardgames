f00d4f860d76b169c014a20c3b52662baa3d47c3
Raphaël Péné <rafdu08@gmail.com>
2026-05-25 16:01:55 +0200

    fonction recopiage de colonne

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.utils import get_data, join_df, choose_columns, clean_data, save_data
 from src.config import URL1, URL2, URL3, COLUMNS, OBJECT_FLOAT, FILL_NA, NEW_TYPE
-from src.excel import create_ws
+from src.excel import load_wb, create_ws, scatter_chart, copy_columns
 import time
 
 
@@ -25,8 +25,21 @@ def main():
         save_data(df_main)
         print(f"Données exportées au format .xlsx en {time.perf_counter()-t:.2f}s")
 
-        ws=create_ws("template.xlsx","TDB","test.xlsx")
-        print(f"Feuille TDB créée en {time.perf_counter()-t:.2f}s")
+        wb=load_wb("template.xlsx")
+        print("Workbook chargé")
+
+        create_ws(wb,"TDB","test.xlsx")
+        print("Feuille TDB créée")
+
+        create_ws(wb,"SCATTER","test.xlsx")
+        print("Feuille SCATTER créée")
+
+        copy_columns(wb,wb["DATA"],wb["SCATTER"], col=9,min_row=2, save_as="test.xlsx")
+
+        #il faut que je copie les données qui m'interessent dans une autre feuille
+
+
+        #scatter_chart(wb["TDB"],wb["DATA"],)
     
     except Exception as e:
         print(f"Erreur : {type(e).__name__}: {e}")
--- src/excel.py
+++ src/excel.py
@@ -1,18 +1,47 @@
 from openpyxl import load_workbook
+from openpyxl import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.chart import (
+    LineChart,
+    Reference,
+)
 
-def create_ws(path: str, sheet_name: str, save_as: str) -> Worksheet:
-    """Création d'une feuille excel
+def load_wb(path: str) -> Workbook:
+    return load_workbook(filename = path)
 
-    Args:
-        path (str): fichier excel à charger
-        sheet_name (str): nom de la feuille
-        save_as (str): fichier excel de sortie
 
-    Returns:
-        Worksheet: feuille créée
-    """
-    wb = load_workbook(filename = path)
+def create_ws(wb: Workbook, sheet_name: str, save_as: str):
     ws = wb.create_sheet(sheet_name, 0)
     wb.save(save_as)
-    return ws
+    # je ne sais pas s'il vaut pas mieux retrouner le wb pour pouvoir l'utiliser au global
+    #return ws
+
+def copy_columns(wb: Workbook, ws_data: Worksheet, ws_destination: Worksheet, col: int, min_row: int, save_as: str):
+    for row in ws_data.iter_rows(min_row=min_row, max_row=ws_data.max_row):
+        val=row[col-1].value
+        ws_destination.append([val])
+    wb.save(save_as)
+
+
+
+def scatter_chart(wb: Workbook, worksheet_chart: Worksheet, worksheet_data: Worksheet, where: str, min_col: int, max_col: int, min_row: int, max_row: int, save_as: str):
+    c1 = LineChart()
+    c1.title = "Complexité vs Note utilisateur"
+    c1.style = 13
+    c1.y_axis.title = 'Note utilisateur'
+    c1.x_axis.title = 'Complexité'
+
+    data = Reference(worksheet_data, min_col=min_col, min_row=min_row, max_col=max_col, max_row=max_row)
+    c1.add_data(data, titles_from_data=True)
+
+    s1 = c1.series[0]
+    s1.graphicalProperties.line.solidFill = "00AAAA"
+    s1.graphicalProperties.line.dashStyle = "sysDot"
+    s1.graphicalProperties.line.width = 100050 # width in EMUs
+
+    worksheet_chart.add_chart(c1, where)
+
+    wb.save(save_as)
+
+    # a voir si  vaut mieux pas retourner le workbook ou rien retourner
+    #return worksheet_chart
